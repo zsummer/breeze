f51942f964c368c97fa573172698255a431cb995
zhangyawei <yawei_zhang@foxmail.com>
2014-11-16 00:44:04 +0800

    create account info

--- db/db_info.sql
+++ db/db_info.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 10.0.7.21
-Source Server Version : 50149
-Source Host           : 10.0.7.21:3306
+Source Server         : 127.0.0.1
+Source Server Version : 50150
+Source Host           : 127.0.0.1:3306
 Source Database       : db_info
 
 Target Server Type    : MYSQL
-Target Server Version : 50149
+Target Server Version : 50150
 File Encoding         : 65001
 
-Date: 2014-11-05 19:37:46
+Date: 2014-11-16 00:43:09
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -36,6 +36,7 @@ INSERT INTO tb_account VALUES ('2', 'zhangyawei2', '0', '0', '0', '0');
 INSERT INTO tb_account VALUES ('5', '', '0', '0', '0', '0');
 INSERT INTO tb_account VALUES ('6', '', '0', '0', '0', '0');
 INSERT INTO tb_account VALUES ('8', '', '0', '0', '0', '0');
+INSERT INTO tb_account VALUES ('3', 'zhangyawei0000', '0', '0', '0', '0');
 
 -- ----------------------------
 -- Table structure for `tb_character`
@@ -48,40 +49,34 @@ CREATE TABLE `tb_character` (
   `iconID` int(10) unsigned NOT NULL DEFAULT '0',
   `level` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`charID`),
-  UNIQUE KEY `accID` (`accID`)
+  KEY `accID` (`accID`) USING BTREE
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of tb_character
 -- ----------------------------
+INSERT INTO tb_character VALUES ('0', '3', 'fd', '0', '0');
+INSERT INTO tb_character VALUES ('2', '3', 'ddd', '0', '0');
+INSERT INTO tb_character VALUES ('3', '3', 'eeee', '0', '0');
 
 -- ----------------------------
 -- Procedure structure for `AutoSelectAccount`
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `AutoSelectAccount`;
 DELIMITER ;;
-CREATE DEFINER=`root`@`%` PROCEDURE `AutoSelectAccount`(IN `v_accID` bigint)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AutoSelectAccount`(IN `v_accID` bigint)
 tag:BEGIN 
 	declare v_accName varchar(20);
-  declare v_diamond  int(10) unsigned;
-  declare v_giftDmd int(10) unsigned;
-  declare v_hisDiamond int(10) unsigned;
-  declare v_hisGiftDmd int(10) unsigned;
 
-	select a.accName, a.diamond, a.giftDmd, a.hisDiamond, a.hisGiftDmd 
-		into v_accName, v_diamond,v_giftDmd,  v_hisDiamond,v_hisGiftDmd 
-		from tb_account a where a.accID = v_accID;
+	select a.accName into v_accName from tb_account a where a.accID = v_accID;
 
-  
+
 	if( v_accName is NULL) THEN
-		 select "", 0,0,0,0 into v_accName, v_diamond,v_giftDmd,  v_hisDiamond,v_hisGiftDmd ;
-		 insert into tb_account(accID, accName, diamond, giftDmd, hisDiamond, hisGiftDmd) 
-				values(v_accID, v_accName, v_diamond,v_giftDmd,  v_hisDiamond,v_hisGiftDmd );
+		 insert into tb_account(accID, accName, diamond, giftDmd, hisDiamond, hisGiftDmd) values(v_accID, "", 0,0,0,0);
 	end if;
 				
-  select v_accName, v_diamond,v_giftDmd,  v_hisDiamond,v_hisGiftDmd,
-				c.charID, c.charName, c.iconID, c.`level` from tb_character c where c.accID = v_accID;
-				
+	select a.accName, a.diamond, a.giftDmd, a.hisDiamond, a.hisGiftDmd, c.charID, c.charName, c.iconID, c.`level` 
+		from tb_account a  left join tb_character c on a.accID = c.accID where a.accID = v_accID;
 END
 ;;
 DELIMITER ;
