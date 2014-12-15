7b4d7d548a500cb54da04cf4d3e2778cf8d67789
zhangyawei <yawei_zhang@foxmail.com>
2014-12-15 09:20:27 +0800

    update cmake

--- source/bin/run.sh
+++ source/bin/run.sh
@@ -1,25 +1,7 @@
 #!/usr/bin/bash
-cd auth
-./auth 0  2>/dev/null 1>&2 &
-./auth 1  2>/dev/null 1>&2 &
-ps -ef |grep "\bauth\b" |grep -v "grep"
+cd mini-breeze
+./mini-breeze 0  2>/dev/null 1>&2 &
+ps -ef |grep "\bmini-breeze\b" |grep -v "grep"
 sleep 2
 
 
-cd ../logic
-./logic 0  2>/dev/null 1>&2 &
-ps -ef |grep "\blogic\b" |grep -v "grep"
-sleep 2
-
-cd ../center
-./center 0  2>/dev/null 1>&2 &
-ps -ef |grep "\bcenter\b" | grep -v "grep"
-sleep 2
-
-
-cd ../agent
-./agent 0  2>/dev/null 1>&2 &
-./agent 1  2>/dev/null 1>&2 &
-ps -ef |grep "\bagent\b" | grep -v "grep"
-
-sleep 2
