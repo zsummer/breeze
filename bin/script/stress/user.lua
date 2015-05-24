7a23dcb3c51a64b330af451a7af522ee8e98f13a
张亚伟 <yawei.zhang@foxmail.com>
2015-05-24 16:46:33 +0800

    update

--- bin/script/stress/user.lua
+++ bin/script/stress/user.lua
@@ -86,29 +86,54 @@ function User:on_LinkServerAck(sID, msg)
 	    return
 	end
     logi("login success. account=" .. self.account .. ", nickName=" .. self.userinfo.nickName)
+
+    --获取自己的交友信息
 	local data = Proto4z.encode({uID=self.uID}, "GetContactInfoReq")
     summer.sendContent(sID, Proto4z.GetContactInfoReq.__getID, data) 
 end
 
+-- 保存服务器端传来的交友信息
 function User:on_GetContactInfoAck(sID, msg)
-
 	if msg.retCode ~= Proto4z.EC_SUCCESS then
 		loge("on_GetContactInfoAck retcode ~= EC_SUCCESS. ret=" .. msg.retCode)
 	    return
 	end
+   
+    self.contact = msg.contact
+
     logi("on_GetContactInfoAck success. totalFriends=" .. msg.contact.totalFriends )
-	dump(msg, "on_GetContactInfoAck")
-	local data = Proto4z.encode({}, "GetSomeStrangersReq")
-    summer.sendContent(sID, Proto4z.GetSomeStrangersReq.__getID, data) 
+    dump(msg, "on_GetContactInfoAck")
+
+    --获取一些在线的陌生人ID
+    if msg.contact.totalFriends == 0 then
+    	local data = Proto4z.encode({}, "GetSomeStrangersReq")
+        summer.sendContent(sID, Proto4z.GetSomeStrangersReq.__getID, data) 
+    end
 end
 
 function User:on_GetSomeStrangersAck(sID, msg)
-
 	if msg.retCode ~= Proto4z.EC_SUCCESS then
 		loge("on_GetSomeStrangersAck retcode ~= EC_SUCCESS. ret=" .. msg.retCode)
 	    return
 	end
     logi("on_GetSomeStrangersAck success. #uIDs=" .. #msg.uIDs )
 	dump(msg, "on_GetSomeStrangersAck")
+    for _, uID in ipairs(msg.uIDs) do
+        local data = Proto4z.encode({uID=uID, oFlag=Proto4z.FRIEND_ADDFRIEND}, "FriendOperationReq")
+        summer.sendContent(sID, Proto4z.FriendOperationReq.__getID, data) 
+    end
 end
 
+function User:on_FriendOperationAck(sID, msg)
+	if msg.retCode ~= Proto4z.EC_SUCCESS then
+		loge("on_GetSomeStrangersAck retcode ~= EC_SUCCESS. ret=" .. msg.retCode)
+	    return
+	end
+    logi("on_FriendOperationAck success. srcUID=" .. msg.srcUID .. ", srcFlag=" .. msg.srcFlag .. ", dstUID=" .. msg.dstUID )
+	if msg.dstUID == self.uID then
+	    if msg.srcFlag == Proto4z.FRIEND_ADDFRIEND then
+            local data = Proto4z.encode({uID=msg.srcUID, oFlag=Proto4z.FRIEND_ALLOW}, "FriendOperationReq")
+            summer.sendContent(sID, Proto4z.FriendOperationReq.__getID, data) 
+        end
+	end
+end
