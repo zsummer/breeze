a6a0f1ee803611215d910af1ef8147c6a6c6c3db
zsummer <yawei.zhang@foxmail.com>
2015-10-03 00:07:13 +0800

    负索引

--- source/common/lua/summer.cpp
+++ source/common/lua/summer.cpp
@@ -222,8 +222,8 @@ static int stop(lua_State *L)
 
 static int runOnce(lua_State * L)
 {
-    int isImmediately = lua_toboolean(L, -1);
-    bool ret = SessionManager::getRef().runOnce(!!isImmediately);
+    int isImmediately = lua_toboolean(L, 1);
+    bool ret = SessionManager::getRef().runOnce(isImmediately);
     lua_pushboolean(L, ret);
     return 1;
 }
