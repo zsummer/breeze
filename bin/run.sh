6cdd3f4e50301844862c2962badede1d0e3cc6f8
zhangyawei <yawei_zhang@foxmail.com>
2016-06-16 21:35:06 +0800

    valgrind test

--- bin/run.sh
+++ bin/run.sh
@@ -2,6 +2,17 @@
 cd docker
 ./docker 1   &
 ./docker 2   &
+#valgrind --tool=massif --trace-children=yes --time-unit=B --max-snapshots=100 --pages-as-heap=yes --detailed-freq=1000 --massif-out-file=docker1.massif.out.%p ./docker 1 &
+#valgrind --tool=massif --trace-children=yes --time-unit=B --max-snapshots=100 --pages-as-heap=yes --detailed-freq=1000 --massif-out-file=docker2.massif.out.%p ./docker 2 &
+
+#valgrind --tool=callgrind --separate-threads=yes ./docker 1 &
+#valgrind --tool=callgrind --separate-threads=yes ./docker 2 &
+
+
+#valgrind --tool=callgrind  --trace-children=yes ./docker 1 &
+#valgrind --tool=callgrind  --trace-children=yes ./docker 2 &
+
+
 ps -ef |grep "\bdocker\b" |grep -v "grep"
 sleep 2
 
