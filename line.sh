574d4c5ad9f1c0ace3f382b2f69de5ac042abaf9
zsummer <zhangyawei@zhangyawei.local>
2016-07-03 16:34:21 +0800

    line

--- line.sh
+++ line.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-common=`find ./source/common -maxdepth 1 -type f \( -name "*.cpp" -or -name "*.h" \)  -print0 | xargs -0 cat |grep -v -e ^// -e ^$ -e "^[ ]\*" -e "$\*"| wc -l`
-docker=`find ./source/docker -type f \( -name "*.cpp" -or -name "*.h" \)  -print0 | xargs -0 cat |grep -v -e ^// -e ^$ -e "^[ ]\*" -e "$\*"| wc -l`
-world=`find ./source/world -type f \( -name "*.cpp" -or -name "*.h" \)  -print0 | xargs -0 cat |grep -v -e ^// -e ^$ -e "^[ ]\*" -e "$\*"| wc -l`
-echo "total common=$common  docker=$docker world=$world"
+common=`find ./source/common -maxdepth 1 -type f \( -name "*.cpp" -or -name "*.h" \)  -print0 | xargs -0 cat |grep -v -e ^// -e ^$ -e "^[ ]\*" -e "$\*"| wc -l |awk '{print $1}'`
+docker=`find ./source/docker -type f \( -name "*.cpp" -or -name "*.h" \)  -print0 | xargs -0 cat |grep -v -e ^// -e ^$ -e "^[ ]\*" -e "$\*"| wc -l|awk '{print $1}'`
+world=`find ./source/world -type f \( -name "*.cpp" -or -name "*.h" \)  -print0 | xargs -0 cat |grep -v -e ^// -e ^$ -e "^[ ]\*" -e "$\*"| wc -l|awk '{print $1}'`
+echo "total common=$common,  docker=$docker,  world=$world"
