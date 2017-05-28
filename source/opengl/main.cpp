01f9e51dd5cb487ac0a3cf00da36854841f4b63b
zsummer <yawei.zhang@foxmail.com>
2017-05-29 00:32:17 +0800

    opengl

---
+++ source/opengl/main.cpp
@@ -0,0 +1,304 @@
+﻿/*
+* breeze License
+* Copyright (C) 2014-2017 YaweiZhang <yawei.zhang@foxmail.com>.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#define _GLFW_WIN32
+#include <common.h>
+#include <utls.h>
+#include <balance.h>
+using namespace zsummer::log4z;
+using namespace zsummer::mysql;
+ 
+#include <glad/glad.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+#include <include/GLFW/glfw3.h>
+#include <include/GLFW/glfw3native.h>
+#include <linmath.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#pragma comment(lib, "OpenGL32")
+#pragma comment(lib, "GLu32")
+
+
+
+static const struct
+{
+    float x, y;
+    float r, g, b;
+} vertices[3] =
+{
+    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
+    { 0.6f, -0.4f, 0.f, 1.f, 0.f },
+    { 0.f,  0.6f, 0.f, 0.f, 1.f }
+};
+
+static const char* vertex_shader_text =
+"#version 110\n"
+"uniform mat4 MVP;\n"
+"attribute vec3 vCol;\n"
+"attribute vec2 vPos;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    color = vCol;\n"
+"}\n";
+
+static const char* fragment_shader_text =
+"#version 110\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_FragColor = vec4(color, 1.0);\n"
+"}\n";
+
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main(void)
+{
+    GLFWwindow* window;
+    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    GLint mvp_location, vpos_location, vcol_location;
+
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glfwSwapInterval(1);
+
+    // NOTE: OpenGL error checks have been omitted for brevity
+
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShader(vertex_shader);
+
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShader(fragment_shader);
+
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    mvp_location = glGetUniformLocation(program, "MVP");
+    vpos_location = glGetAttribLocation(program, "vPos");
+    vcol_location = glGetAttribLocation(program, "vCol");
+
+    glEnableVertexAttribArray(vpos_location);
+    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+        sizeof(vertices[0]), (void*)0);
+    glEnableVertexAttribArray(vcol_location);
+    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+        sizeof(vertices[0]), (void*)(sizeof(float) * 2));
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int width, height;
+        mat4x4 m, p, mvp;
+
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float)height;
+
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        mat4x4_identity(m);
+        mat4x4_rotate_Z(m, m, (float)glfwGetTime());
+        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        mat4x4_mul(mvp, p, m);
+
+        glUseProgram(program);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
+
+
+
+/*
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+
+void error_callback(int error, const char *description)
+{
+    LOGE(description);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    std::cout << key << std::endl;
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+
+int main(int argc, char* argv[])
+{
+
+#ifndef _WIN32
+    //! linux下需要屏蔽的一些信号
+    signal( SIGHUP, SIG_IGN );
+    signal( SIGALRM, SIG_IGN ); 
+    signal( SIGPIPE, SIG_IGN );
+    signal( SIGXCPU, SIG_IGN );
+    signal( SIGXFSZ, SIG_IGN );
+    signal( SIGPROF, SIG_IGN ); 
+    signal( SIGVTALRM, SIG_IGN );
+    signal( SIGQUIT, SIG_IGN );
+    signal( SIGCHLD, SIG_IGN);
+    setenv("TZ", "GMT-8", 1);
+#else
+    //system("chcp 65001");
+#endif
+    srand((ui32)time(NULL));
+
+
+    ILog4zManager::getPtr()->start();
+    SessionManager::getRef().start();
+
+    glfwSetErrorCallback(error_callback);
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    if (!window)
+    {
+        LOGE("Failed to create GLFW window");
+        glfwTerminate();
+        return -1;
+    }
+    glfwSetKeyCallback(window, key_callback);
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glfwSwapInterval(1);
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);      // 玄色配景 
+    glColor3f(1.0f, 1.0f, 1.0f);
+
+    glShadeModel(GL_FLAT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-50.0f, 50.0f, -50.0f, 50.0f, -1.0f, 1.0f);
+
+
+    while (!glfwWindowShouldClose(window))
+    { 
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float)height;
+        glViewport(0, 0, width, height);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 0.f, 1.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+
+        glBegin(GL_LINE);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 0.f, 1.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+
+        static  float  fSpin = 0.0f;
+
+        fSpin += 2.0f;
+
+        if (fSpin > 360.0f) {
+            fSpin -= 360.0f;
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glPushMatrix();
+
+        // 扭转矩形的重要函数 
+        glRotatef(fSpin, 0.0f, 0.0f, 1.0f);
+        glRectf(-25.0, -25.0, 25.0, 25.0);
+        glPopMatrix();
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    LOGA("check all success.");
+    return 0;
+}
+*/
---
+++ tools/glfw.md
@@ -0,0 +1,1 @@
+> https://github.com/glfw/glfw.git
