b498a6aaa49fe29814aabd56cde30bba0cb79f45
zhangyawei <yawei_zhang@foxmail.com>
2017-07-27 20:16:57 +0800

    Julia set

--- source/opengl/main.cpp
+++ source/opengl/main.cpp
@@ -38,38 +38,6 @@ using namespace zsummer::mysql;
 #pragma comment(lib, "GLu32")
 
 
-
-static const struct
-{
-    float x, y;
-    float r, g, b;
-} vertices[3] =
-{
-    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
-    { 0.6f, -0.4f, 0.f, 1.f, 0.f },
-    { 0.f,  0.6f, 0.f, 0.f, 1.f }
-};
-
-static const char* vertex_shader_text =
-"#version 110\n"
-"uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
-
-static const char* fragment_shader_text =
-"#version 110\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_FragColor = vec4(color, 1.0);\n"
-"}\n";
-
 static void error_callback(int error, const char* description)
 {
     fprintf(stderr, "Error: %s\n", description);
@@ -111,8 +79,8 @@ int main(void)
 
     if (!glfwInit())
         exit(EXIT_FAILURE);
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
     window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
@@ -128,70 +96,13 @@ int main(void)
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     glfwSwapInterval(1);
 
-    // NOTE: OpenGL error checks have been omitted for brevity
-    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
-    GLint mvp_location, vpos_location, vcol_location;
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-    glCompileShader(vertex_shader);
-
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-    glCompileShader(fragment_shader);
-
-    program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-    glLinkProgram(program);
-
-    mvp_location = glGetUniformLocation(program, "MVP");
-    vpos_location = glGetAttribLocation(program, "vPos");
-    vcol_location = glGetAttribLocation(program, "vCol");
-
-    glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
-        sizeof(vertices[0]), (void*)0);
-    glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
-        sizeof(vertices[0]), (void*)(sizeof(float) * 2));
+    LOGI("GL_VERSION:" << (const char*)glGetString(GL_VERSION));
 
     while (!glfwWindowShouldClose(window))
     {
-        float ratio;
-        int width, height;
-        mat4x4 m, p, mvp;
+        glShadeModel(GL_SMOOTH);
 
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float)height;
-
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float)glfwGetTime());
-        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        mat4x4_mul(mvp, p, m);
-
-        glUseProgram(program);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-
-        glBegin(GL_LINE);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
-        glEnd();
-
-        glClearColor(0.0, 0.0, 0.0, 0.0);
-        glClear(GL_COLOR_BUFFER_BIT);
+        /* Draw a triangle */
         glBegin(GL_TRIANGLES);
 
         glColor3f(1.0, 0.0, 0.0);    // Red
@@ -202,14 +113,78 @@ int main(void)
 
         glColor3f(0.0, 0.0, 1.0);    // Blue
         glVertex3f(1.0, -1.0, 0.0);
+
         glEnd();
+
+
+        //glEnable(GL_LINE_STIPPLE);
+        //glLineStipple(1, 1);
+        glColor3f(1.0, 1.0, 1.0);    // Red
+        glLineWidth(1);
+
+
+        glBegin(GL_LINES);
+        glVertex2f(0, 0);
+        glColor3f(1.0, 0.0, 1.0);    // Red
+        glVertex2f(1,1);
+        glEnd();
+
+
+        glBegin(GL_LINES);
+        glVertex3f(0, 1, 0);
+        glColor3f(0.0, 0.0, 1.0);    // Red
+        glVertex3f(1, 0, 1);
+        glEnd();
+
+
+        glBegin(GL_POINTS);
+        glColor3f(0.0, 1.0f, 1.0f);   
+        glVertex3f(0, 0, 0);
+        glEnd();
+
+        glBegin(GL_POINTS);
+        glColor3f(0.0, 1.0f, 1.0f);
+        glVertex3f(0.8f, 0.8f, 0.8f);
+        glEnd();
+
+        glBegin(GL_POINTS);
+        glColor3f(0.0, 1.0f, 1.0f);
+        glVertex3f(0, 0.8f, 0);
+        glEnd();
+
+        glBegin(GL_POINTS);
+        glColor3f(0.0, 1.0f, 1.0f);
+        glVertex3f(0, 0, 0.8f);
+        glEnd();
+
+        glBegin(GL_POINTS);
+        glColor3f(0.0, 1.0f, 1.0f);
+        glVertex3f(0.8f, 0, 0);
+        glEnd();
+
+        glBegin(GL_POINTS);
+        glColor3f(0.0, 1.0f, 1.0f);
+        glVertex3f(0.8f, 0.8f, 0);
+        glEnd();
+
+        glBegin(GL_POINTS);
+        glColor3f(0.0, 1.0f, 1.0f);
+        glVertex3f(0, 0.8f, 0.8f);
+        glEnd();
+
+        glBegin(GL_POINTS);
+        glColor3f(0.0, 1.0f, 1.0f);
+        glVertex3f(0.8f, 0, 0.8f);
+        glEnd();
+
+
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwDestroyWindow(window);
-
+    glGetError();
     glfwTerminate();
     exit(EXIT_SUCCESS);
 }
